2e41a3444c7f7a1f19b82fda7fc7ba40bb7774ff
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-05-04 15:24:55 -0300

    Execute process initial structure * Organized program structure with helpful comments. * Added argument handling.

--- src/execute.c
+++ src/execute.c
@@ -1,15 +1,34 @@
+// Program scheduler - Execution process.
+
+// Includes:
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// User includes:
 #include "../include/console.h"
 
+// Main function:
 int main(int argc, char **argv){
 
-    int a = 2;
+  // Variable declaration:
+  char context[20] = "Execute process", program_name[255];
+  unsigned int delay;
 
-    warning("Execute process", "Task done %d\n", a);
+  // Argument handling:
+  if(argc != 3) {
+    error(context,
+          "Wrong argument count. Usage: ./execute program_name delay.\n");
+    exit(1);
+  }
 
-    // available messages: success (green), error (red), warning (yellow), info (blue), message (default print)
+  strcpy(program_name, argv[1]);
+  delay = atoi(argv[2]);
 
+  success(context,
+          "Program '%s' scheduled for execution in at least %u seconds!\n",
+          program_name, delay);
 
+  return 0;
 
-    return 0;
 }
