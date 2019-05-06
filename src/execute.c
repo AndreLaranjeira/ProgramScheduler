e9287e8c72ace70d3d856114df337c6a04a9be59
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-05-06 08:09:06 -0300

    Execute process - Arguments for delayed execution program * You can now schedule a delayed execution for a program with mulitple arguments.

--- src/execute.c
+++ src/execute.c
@@ -12,22 +12,30 @@
 int main(int argc, char **argv){
 
   // Variable declaration:
-  char context[20] = "Execute process", program_name[255];
+  char context[20] = "Execute process", program_and_args[512];
+  int i;
   unsigned int delay;
 
   // Argument handling:
-  if(argc != 3) {
+  if(argc < 3) {
     error(context,
-          "Wrong argument count. Usage: ./execute program_name delay.\n");
+          "Wrong argument count. Usage: ./execute program [args] delay.\n");
     exit(1);
   }
 
-  strcpy(program_name, argv[1]);
-  delay = atoi(argv[2]);
+  strcpy(program_and_args, argv[1]);    // First, copy the program name.
+
+  // Now, copy the program arguments, if there are any.
+  for(i = 2; i < argc - 1; i++) {
+    strcat(program_and_args, " ");
+    strcat(program_and_args, argv[i]);
+  }
+
+  delay = atoi(argv[argc - 1]);         // Delay is always the last argument.
 
   success(context,
           "Program '%s' scheduled for execution in at least %u seconds!\n",
-          program_name, delay);
+          program_and_args, delay);
 
   return 0;
 
