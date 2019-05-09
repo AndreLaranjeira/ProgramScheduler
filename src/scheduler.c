610387406305199872bbfcd0acc8c58f75ed763a
01oseluiz <joseluizgnogueira@live.com>
2019-05-09 01:11:47 -0300

    Done initialization of the messages queue

--- src/scheduler.c
+++ src/scheduler.c
@@ -4,19 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+// - To messages queues
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
 // User includes:
 #include "../include/console.h"
-
-// Main function:
 #include "../include/data_structures.h"
 
+// Main function:
 int main(int argc, char **argv){
 
     // Variables declaration
     const char context[] = "Scheduler";
     const char arg_1_topologies[] = "hypercube, torus, fat_tree";
     char *selecte_topology;
+    int msqid_top_level, msqid_nodes;
 
     // Arguments number handling:
     if(argc != 2){
@@ -35,8 +40,46 @@ int main(int argc, char **argv){
                 "...Starting scheduler with %s topology\n", selecte_topology);
     }
 
-    // Create queues
-    // TODO- create queues
+    // Create messages queue for shutdown, execute and scheduler to communicate
+    if( (msqid_top_level = msgget(QUEUE_TOP_LEVEL, IPC_CREAT|0x1FF)) != -1 ){
+        info(context,
+                "Messages queue for shutdown, execute and scheduler created with success!\n");
+    }else{
+        warning(context,
+             "An error occur trying to create a messages queue for shutdown, execute and scheduler !\n");
+    }
+
+    // Create messages queue for nodes and scheduler to communicate
+    if( (msqid_nodes = msgget(QUEUE_NODES, IPC_CREAT|0x1FF)) != -1 ){
+        info(context,
+             "Messages queue for nodes and scheduler created with success!\n");
+    }else{
+        warning(context,
+                "An error occur trying to create a messages queue for nodes and scheduler !\n");
+    }
+
+
+    // TODO - neste ponto a fila esta criada use-a com sabedoria!
+
+
+    // Destroy messages queue of shutdown, execute and scheduler
+    if(msgctl(msqid_top_level, IPC_RMID, NULL) != -1){
+        info(context,
+             "Messages queue for shutdown, execute and scheduler destroyed with success!\n");
+    }else{
+        warning(context,
+                "An error occur trying to destroy a messages queue for shutdown, execute and scheduler !\n");
+    }
+
+    // Destroy messages queue of nodes and scheduler
+    if(msgctl(msqid_nodes, IPC_RMID, NULL) != -1){
+        info(context,
+             "Messages queue for nodes and scheduler destroyed with success!\n");
+    }else{
+        warning(context,
+                "An error occur trying to destroy a messages queue for nodes and scheduler !\n");
+    }
+
 
     return 0;
 
